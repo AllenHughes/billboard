a239cff22e62297d7c8f94d4b9fa0a642fa21c6f
Allen Hughes <allen@brushycreekchurch.com>
2017-07-31 16:48:38 -0400

    Added a lot of functiuonality; announcement-list-entries currently fails to compile on my windows machine

--- billboard.el
+++ billboard.el
@@ -12,7 +12,7 @@
 
 (defun add-announcement (announcement) (push announcement *announcements*))
 
-(defun prompt-new-announcemnt (ti co dl de no pr)
+(defun prompt-new-announcement (ti co dl de no pr)
   "Interactively get the values for a new announcement and add to db."
   (interactive "MTitle: \nMContact: \nMDealine: \nMDescription: \nMNotes: \nMPriority: ")
   (add-announcement (make-announcement ti co dl de no pr)))
@@ -42,43 +42,103 @@
 	 (concat (propertize "Billboard: " 'face 'success)
 		 (propertize "Keep track of Brushy Creek announcmnets" 'face 'success)))))
 
+(defun announcement-list-entries ()
+  "This function is used build a list of entries
+
+It creates a list of list of list, each having an id for the first element
+and a vector of objects for the second element. It currently uses the 
+global variable '*announcements', but it might be useful at some point to
+differnt datastore"
+  (let (entries)
+    (dolist (anncmnt *announcements*)
+      (let* ((id (cdr (assoc 'id anncmnt)))
+	     (title (cdr (assoc 'title anncmnt)))
+	     (deadline (cdr (assoc 'deadline anncmnt)))
+	     (contact (cdr (assoc 'contact anncmnt)))
+	     (notes (if (eq (cdr (assoc 'notes anncmnt)) nil)
+			" "
+		      "Y"))
+	     (priority (let ((value (cdr (assoc 'priority anncmnt))))
+			 (cond
+			  ((equal value nil) " ")
+			  ((equal value "LOW") "Low")
+			  ((equal value "MED") (propertize "Med" 'face 'warning))
+			  ((equal value "HIGH") (propertize "High" 'face 'error))
+			  (t "t")))))
+	     (push (list id
+			 (vector " "
+				 title
+				 deadline
+				 contact
+				 notes
+				 priority)))
+		   entries)))
+      (setq tabulated-list-entries entries))
+
+(defvar billboard-list-mode-map
+  (let ((map (make-sparse-keymap)))
+    (set-keymap-parent map tabulated-list-mode-map)
+    (define-key map (kbd "N") 'prompt-new-announcement)
+    map))
 
 (define-derived-mode billboard-list-mode
   tabulated-list-mode "Billboard List"
   "Major mode for displaying a list of announcments"
 
   (setq tabulated-list-format
-	[("Title" 30 nil)
+	[(" " 1 nil)
+	 ("Title" 30 nil)
 	 ("Deadline" 12 t)
 	 ("Contact" 20 t)
 	 ("Notes" 5 t)
-	 ("Description" 40 nil)])
+	 ("Priority" 10 t)])
+  (use-local-map billboard-list-mode-map)
+  (add-hook 'tabulated-list-revert-hook 'announcement-list-entries nil t)
   (tabulated-list-init-header))
 
 ;; This is the actual command to create the list buffer
-(defun billboard-list ()
+(defun billboard ()
   "Create buffer for list and change major mode"
   (interactive)
   (let ((new-buffer (get-buffer-create "*Annoucements*")))
     (save-current-buffer
       (set-buffer new-buffer)
       (billboard-list-mode)
+      (announcement-list-entries)
       (tabulated-list-print)
       (switch-to-buffer new-buffer))))
 
 ;; For testing 
 (defun clear-vars ()
+  (interactive)
   (makunbound '*announcements*))
 
-(defun reload-announcements ()
-    (setq *announcements*
-	  (((contact . "Allen Hughes")
-	    (title . "Another thing")
-	    (deadline . "2017-08-20")
-	    (description . "Stuff and things")
-	    (notes . "Nothing"))
-	   ((contact . "Pattsy Wilson")
-	    (title . "Wedding Shower for Allen and Haleigh")
-	    (deadline . "2017-07-16")
-	    (description . "What is says")
-	    (notes . "noNotes")))))
+(setq *announcements*
+      '(((id . 20170720574A)
+	 (contact . "Allen Hughes")
+	 (title . "Another thing")
+	 (deadline . "2017-08-20")
+	 (description . "Stuff and things")
+	 (priority . "HIGH")
+	 (notes . "Nothing"))
+	((id . 20170723584B)
+	 (contact . "Pattsy Wilson")
+	 (title . "Wedding Shower for Allen and Haleigh")
+	 (deadline . "2017-07-16")
+	 (description . "What is says")
+	 (notes . "noNotes")
+	 (priority . "LOW"))
+	((id . 20170724884B)
+	 (contact . "Haleigh Hughes")
+	 (title . "A new thing were doing")
+	 (deadline . "2017-08-23")
+	 (description . "Another thing")
+	 (priority . "MED")
+	 (notes . nil))
+      	((id . 2017072488548743FD)
+	 (contact . "LInda Hughes")
+	 (title . "Kniting")
+	 (deadline . "2017-10-23")
+	 (priority . "LOW")
+	 (description . "A thing were the yall get together and knit")
+	 (notes . nil))))
