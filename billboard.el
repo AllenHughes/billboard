1be9939dc155db66f9ed970c85739af2b04743b7
Allen Hughes <allen@brushycreekchurch.com>
2017-08-15 15:54:59 -0400

    Completed 'find-sundays'

--- billboard.el
+++ billboard.el
@@ -256,31 +256,37 @@ differnt datastore"
   (interactive
    (list
     (apply #'encode-time (parse-time-string (concat (read-string "Display every sunday until: ") " 00:00:00")))))
-  (let* ((sundays (find-sundays current-time arg))) ;'find-sundays' takes start-date and end-date and returns a list of dates for every sunday in that range.
-    (let (all-bulletins)
-      (dolist (sunday sundays all-bulletins)
-	(cons (build-bulletin sunday) all-bulletins))
-      (let ((new-buffer (get-buffer-create "*Upcoming Bulletins*")))
+    (let ((new-buffer (get-buffer-create "*Upcoming Bulletins*")))
+      (save-current-buffer
 	(set-buffer new-buffer)
-	(billboard-upcoming-mode)
+	(setq-local bulletins (build-bulletins (find-sundays (current-time) arg))) ;TODO: build-bulletins
+	(billboard-upcoming-mode) ;TODO: billboard-upcoming-mode
 	(goto-char (point-min))
-	(billboard-bulltin-display all-bulletins)))))
+	(billboard-bulletins-display bulletins) ;TODO: billboard-bulletins-display
+	(switch-to-buffer new-buffer))))
 
 (defun find-sundays (start-time end-time)
+  ;TODO: Should probaly do something about all the 'decode-time'that I use in this.
+  "Takes start-date and end-date and returns a list of dates for every sunday in that range."
   (let ((date (if (eq (nth 6 (decode-time start-time)) 0)
 		  start-time
 		(encode-time 0 0 0
-			     (+ (+ (- 7 (nth 6 (decode-time start-time))) 0) (nth 3 (decode-time start-time)))
+			     (+ (+ (- 7 (nth 6 (decode-time start-time))) 0) (nth 3 (decode-time start-time))) ; finds the next sundays day of month
 			     (nth 4 (decode-time start-time))
 			     (nth 5 (decode-time start-time)))))
-	(end-date (find-privous-sunday end-time))
+	(end-date (if (eq (nth 6 (decode-time end-time)) 0)
+		      end-time
+		    (encode-time 0 0 0
+				 (- (nth 3 (decode-time end-time)) (nth 6 (decode-time end-time))) ;find previous sunday
+				 (nth 4 (decode-time end-time))
+				 (nth 5 (decode-time end-time)))))
 	(sundays))
-    (while (<= date end-date)
+    (while (<= (time-to-seconds date) (time-to-seconds end-date))
       (setq sundays (cons date sundays))
-      (setq date (encode 0 0 0
-			 (+ (nth 3 (decode-time date)) 7) ;set day of month to next sunday
-			 (nth 4 (decode-time date)) ;set month to the month of date
-			 (nth 5 (decode-time date))))) ;set year to the year of date
+      (setq date (encode-time 0 0 0
+			      (+ (nth 3 (decode-time date)) 7) ;set day of month to next sunday
+			      (nth 4 (decode-time date)) ;set month to the month of date
+			      (nth 5 (decode-time date))))) ;set year to the year of date
     (reverse sundays)))
 
 ;;;
